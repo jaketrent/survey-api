f1c7e6e7970135fddaee36536b72561735098d95
jaketrent <trent.jake@gmail.com>
2018-04-02 07:39:33 -0600

    remove table prefix stuff

--- cmd/api/survey/repo.go
+++ cmd/api/survey/repo.go
@@ -10,7 +10,7 @@ func findAll(db *sql.DB) ([]*Survey, error) {
 	const query = `
 select id
 , description
-from survey_api_survey
+from survey
 `
 	rows, err := db.Query(query)
 	if err != nil {
@@ -31,7 +31,7 @@ from survey_api_survey
 
 func insertSurvey(db *sql.DB, survey *Survey) (*Survey, error) {
 	const query = `
-insert into survey_api_survey
+insert into survey
 ( description
 ) values
 ( $1
@@ -43,7 +43,7 @@ insert into survey_api_survey
 
 func updateSurvey(db *sql.DB, survey *Survey) (*Survey, error) {
 	const query = `
-update survey_api_survey
+update survey
 set description = $1
 where id = $2
 returning description
@@ -65,7 +65,7 @@ func deleteSurvey(db *sql.DB, id int) error {
 	}
 
 	result, err = tx.Exec(`
-delete from survey_api_answer
+delete from answer
 where question_id in (
   select id
   from question
@@ -78,7 +78,7 @@ where question_id in (
 	}
 
 	result, err = tx.Exec(`
-delete from survey_api_question
+delete from question
 where survey_id = $1;
 `, id)
 
@@ -88,7 +88,7 @@ where survey_id = $1;
 	}
 
 	result, err = tx.Exec(`
-delete from survey_api_survey
+delete from survey
 where id = $1;
 `, id)
 	count, err = result.RowsAffected()
--- db/migrations/00001_createsurvey.sql
+++ db/migrations/00001_createsurvey.sql
@@ -3,46 +3,46 @@
 
 begin;
 
-create sequence survey_api_survey_id_seq
+create sequence survey_id_seq
 start with 1
 increment by 1
 no minvalue
 no maxvalue
 cache 1;
 
-create sequence survey_api_question_id_seq
+create sequence question_id_seq
 start with 1
 increment by 1
 no minvalue
 no maxvalue
 cache 1;
 
-create sequence survey_api_answer_id_seq
+create sequence answer_id_seq
 start with 1
 increment by 1
 no minvalue
 no maxvalue
 cache 1;
 
-create table survey_api_survey (
-id int primary key default nextval('survey_api_survey_id_seq'),
+create table survey (
+id int primary key default nextval('survey_id_seq'),
 description text not null,
 created date default now()
 );
 
-create type survey_api_question_type as enum ('freeform');
+create type question_type as enum ('freeform');
 
-create table survey_api_question (
-id int primary key default nextval('survey_api_question_id_seq'),
-survey_id int not null references survey_api_survey(id),
+create table question (
+id int primary key default nextval('question_id_seq'),
+survey_id int not null references survey(id),
 prompt text not null,
-question_type survey_api_question_type default 'freeform',
+question_type question_type default 'freeform',
 created date default now()
 );
 
-create table survey_api_answer (
-id int primary key default nextval('survey_api_answer_id_seq'),
-question_id int not null references survey_api_question(id),
+create table answer (
+id int primary key default nextval('answer_id_seq'),
+question_id int not null references question(id),
 value text not null,
 user_info varchar(255),
 created date default now()
@@ -56,12 +56,12 @@ end;
 
 begin;
 
-drop sequence survey_api_survey_id_seq;
-drop sequence survey_api_question_id_seq;
-drop sequence survey_api_answer_id_seq;
-drop table survey_api_survey;
-drop type survey_api_question_type;
-drop table survey_api_question;
-drop table survey_api_answer;
+drop sequence survey_id_seq;
+drop sequence question_id_seq;
+drop sequence answer_id_seq;
+drop table survey;
+drop type question_type;
+drop table question;
+drop table answer;
 
 end;
